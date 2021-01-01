5a3ad98630c5fc37b8090ee9bfc8e4724c41264c
baptistemcn <baptistemcn@icloud.com>
2021-01-01 20:23:13 +0100

    Updates

--- static/js/main.ac9020f9.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpbaptiste-marcon-website"]=this["webpackJsonpbaptiste-marcon-website"]||[]).push([[0],{10:function(e,t,n){},4:function(e,t,n){"use strict";n.r(t);var c=n(0),s=n(1),i=n.n(s),r=n(3),a=n.n(r);n(10);function o(){return Object(c.jsx)("div",{className:"App",children:Object(c.jsx)("h1",{children:"Baptiste Marcon."})})}a.a.render(Object(c.jsx)(i.a.StrictMode,{children:Object(c.jsx)(o,{})}),document.getElementById("root"))}},[[4,1,2]]]);
-//# sourceMappingURL=main.ac9020f9.chunk.js.map
